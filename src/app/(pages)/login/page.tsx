52aec427aed7070d5eff49cce70d51ee1a561329
relasts <tsovyan@inbox.ru>
2026-01-05 18:29:40 +0300

    fix(#9): authorization

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -48,9 +48,9 @@ export default function LoginPage() {
     e.preventDefault();
 
     const errors: ErrorsData = {};
-    if (password != undefined && password.length < 8) {
-      errors.password = 'Не менее 8 символов';
-    }
+    // if (password != undefined && password.length < 8) {
+    //   errors.password = 'Не менее 8 символов';
+    // }
     setFieldsError(errors);
     if (Object.keys(errors).length != 0) {
       return;
@@ -63,18 +63,15 @@ export default function LoginPage() {
         data: { username, password },
       });
       
-      const { token, user } = res.data;
-      if (!token || !user) throw new Error('Некорректный ответ сервера');
-      dispatch(
-        loginAction({
-          user_id: user.id,
-          user_name: user.name,
-          user_role: user.role,
-        }),
-      );
-
-      router.replace('/');
+      if(res.status === 200) {
+        router.replace('/');
+      }
     } catch (err: any) {
+      if(err?.response?.status === 401) {
+        setFieldsError({ message: 'Неверный логин или пароль' });
+        return;
+      }
+
       const msg =
         err?.response?.data?.message || err?.message || 'Ошибка входа';
       setFieldsError({ message: msg });
